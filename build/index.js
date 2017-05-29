6c3187acdf749d1cd5a7c7ab5a2018b325216866
Chris Sundberg <chris.z.sundberg@teliasonera.com>
2017-05-29 10:02:00 +0200

    Made regex less restrictive and enabled format option

--- build/index.js
+++ build/index.js
@@ -3,53 +3,54 @@
 var postcss = require('postcss');
 var path = require('path');
 var fs = require('fs');
+var glob = require('glob');
 
 module.exports = postcss.plugin('postcss-font-base64', function (options) {
   // handle options here
   options = options || {};
+
   // default options
   options.match = options.match || { 'Scrabble': ['fakefont'] };
   options.format = options.format || ['eot', 'woff', 'woff2', 'ttf'];
+
   // variables
   var CWD = path.resolve(process.cwd());
+
   return function (css, result) {
     // Runs through all of the nodes (declarations) in the css
-    var fontFaces = [];
     css.walkAtRules('font-face', function (fontFace) {
+      var fileTypeRegex = getRegexStringForFileTypes(options.format);
 
-      fontFace.each(function (kids) {
-        var fontValues = [];
-        if (kids.prop === 'src') {
-          var properties = kids.value.split(',');
-          //console.log(properties)
-          for (var i = 0; i < properties.length; i++) {}
-          //console.log(properties[i].split(','))
+      // TODO: Return here if font-family doesn't match options.match
 
-          //console.log(fontValues);
-          //kids.replaceValues(/\w/, 'ost')
-          //console.log('value');
-        }
-        //console.log(fontValues);
-        fontFace.replaceValues(/url\([a-z\.\/\? \# \' \"\-]+\)/, function (attr) {
+      fontFace.replaceValues(new RegExp('url\\("?.+\\.' + fileTypeRegex + '"?\\)'), function (attr) {
+        var fontSource = attr.replace(/(url|"|\(|\)|\?#iefix)/g, '');
 
-          var fontSource = attr.match(/[a-z\.\/\? \# \' \" \-]+(?=[\)])/)[0].replace(/\"+/g, '').replace('?#iefix', '');
-          console.log(fontSource);
-          var res64 = base64Encode(fontSource);
+        // TODO: Return here if font filename doesn't match options.match
 
-          var newUrlStr = 'url(data:application/'.concat(getFormat(attr)).concat(';charset=utf-8;base64').concat(res64).concat(')');
+        var res64 = base64Encode(fontSource);
+        var newUrlStr = 'url(data:'.concat(getMimeType(attr)).concat(';charset=utf-8;base64,').concat(res64).concat(')');
 
-          return newUrlStr;
-        });
+        return (res64 ? newUrlStr : attr);
       });
     });
-    // helper functions
-    function getFormat(attribute) {
 
+    function getRegexStringForFileTypes(fileTypes) {
+      var regex = fileTypes.map(function(fileType) {
+        return ((fileType === 'eot') ? fileType.concat('(\\?#iefix)?') : fileType);
+      })
+      .join('|');
+      return ((regex) ? '(' + regex + ')' : '');
+    }
+
+    // helper functions
+    function getMimeType(attribute) {
       var formats = {
-        '.woff': 'font-woff',
-        '.woff2': 'font-woff2',
-        '.ttf': 'font-tiff',
-        '.eot': 'font-eot'
+        '.woff': 'application/font-woff',
+        '.woff2': 'font/woff2',
+        '.ttf': 'application/font-sfnt',
+        '.eot': 'application/vnd.ms-fontobject',
+        '.otf': 'application/font-sfnt'
       };
 
       var match = '';
@@ -61,14 +62,26 @@ module.exports = postcss.plugin('postcss-font-base64', function (options) {
       return match;
     };
 
-    function getFontList(fontPath) {
-      var listOfFonts = fs.readdirSync(fontPath);
-      return listOfFonts;
+    function base64Encode(file) {
+      if (fs.existsSync(file)) {
+        return readAndEncodeFile(file);
+      } else {
+        // Fallback to glob
+        file = glob.sync('**/' + file)[0]; // Could be smarter
+
+        if (fs.existsSync(file)) {
+          return readAndEncodeFile(file);
+        }
+
+        console.warn(file, 'dose not exist.');
+        return '';
+      }
     }
 
-    function base64Encode(file) {
+    function readAndEncodeFile(file) {
       var bitmap = fs.readFileSync(file);
       return new Buffer(bitmap).toString('base64');
     }
+
   };
 });
